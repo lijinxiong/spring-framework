a9ecd41e73cc1afd278a04b6ce9459955c2429b1
lijinxiong <jx.li@igola.com>
2020-04-26 17:10:03 +0800

    [spring-core] aliasRegistry

--- autoPush.bat
+++ autoPush.bat
@@ -1,5 +1,5 @@
 cd D:\idea\spring-framework
-start /wait git add .
-start /wait git commit -m "this auto commit&push"
-start /wait git push origin master
+git add .
+git commit -m "this auto commit&push"
+git push origin master
 pause
